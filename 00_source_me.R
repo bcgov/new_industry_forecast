ae6f6222a37813d48359506f225e3589b4afb102
Rich-P-Martin <richard.martin@gov.bc.ca>
2026-02-04 15:42:35 -0800

    just the forestry drivers at this point.

--- 00_source_me.R
+++ 00_source_me.R
@@ -1,82 +1,145 @@
+library(bcgovpond)
 library(tidyverse)
 library(readxl)
-library(janitor)
-library(fpp3)
 library(here)
-library(assertthat)
+library(janitor)
 library(conflicted)
 conflicts_prefer(dplyr::filter)
+conflicts_prefer(plotly::layout)
 
-#functions------------------------------------------------
+#takes the data from data_store/add_to_pond and adds it to the pond
+ingest_pond()
 
-assert_file_exists <- function(file_name){
-  assert_that(file.exists(here("data", "current", file_name)),
-              msg=paste("A file with the exact name", file_name, "must be in folder data/current"))
+
+clean <- function(x) {
+  x <- gsub("\u00A0", " ", x)   # non-breaking space
+  x <- gsub("[\t\r\n]", " ", x) # tabs / line breaks
+  x <- trimws(x)
+  x
 }
 
-#make the (recursive) file directory if doesn't exist---------------------
+fuzzy_right_join <- function(x, y, by_x, by_y, max_dist = 3) {
+  crossing(x = x, y = y)|> #all possible pairings (Cartesian product)
+    unnest(c(x, y), names_sep = ".") |> #flatten the paired rows into columns
+    mutate(
+      dist = stringdist::stringdist(
+        .data[[paste0("x.", by_x)]],
+        .data[[paste0("y.", by_y)]],
+        method = "lv"
+      )
+    ) |>
+    group_by(.data[[paste0("y.", by_y)]]) |>  # keep canonical side
+    slice_min(dist, with_ties = FALSE) |>
+    ungroup() |>
+    filter(dist <= max_dist)
+}
 
-if (!dir.exists("data")) {dir.create("data")}
-if (!dir.exists(file.path("data","current"))) {dir.create(file.path("data","current"))}
-if (!dir.exists(file.path("data","old"))) {dir.create(file.path("data","old"))}
-if (!dir.exists("out")) {dir.create("out")}
-if (!dir.exists(file.path("out","current"))) {dir.create(file.path("out","current"))}
-if (!dir.exists(file.path("out","old"))) {dir.create(file.path("out","old"))}
+# Employment data: THE correct industry names and codes.
 
-#are the required files where they are supposed to be?----------------
+employment <-  read_view("historic_lmo_ind_code.xlsx")|>
+  select(code=lmo_ind_code, industry=lmo_detailed_industry, year, value=employment)|>
+  group_by(code, industry)|>
+  nest()|>
+  rename(employment=data)
 
-required_files <- c("constraint.xlsx",
-                    "driver.xlsx",
-                    "historic.xlsx",
-                    "notes.xlsx",
-                    "old.xlsx",
-                    "prop_fcast.csv")
+#read in the files with (potentially) incorrect names
 
-sapply(required_files, assert_file_exists)
-
-#read in historic LFS data-------------------------------------------
-tbbl <- read_excel(here("data","current","historic.xlsx"), skip = 3)|>
-  filter(str_detect(`Lmo Ind Code`, "ind"))|>
+old_forecast_wrong_names <- read_view("stokes_forecast.csv", skip = 3)|>
+  filter(NOC=="#T", Industry!="All industries", `Geographic Area`=="British Columbia")|>
+  select(Industry, starts_with("2"))|>
   pivot_longer(cols = starts_with("2"), names_to = "year")|>
+  mutate(year=as.numeric(year))|>
   clean_names()|>
-  group_by(year)|>
-  mutate(prop=value/sum(value))|>
+  group_by(industry)|>
+  nest()|>
+  rename(old_forecast=data)|>
+  fuzzy_right_join(
+    employment,
+    by_x = "industry",
+    by_y = "industry"
+  )
+
+#names (if any) should refer to same industry
+old_forecast_wrong_names[old_forecast_wrong_names$x.industry!=old_forecast_wrong_names$y.industry,]
+
+driver_data_wrong_names <- read_view("driver.xlsx")|>
+  select(industry=(matches("ind") & !matches("code")), starts_with("2"))|>
+  pivot_longer(cols=starts_with("2"), names_to = "year", values_to = "value")|>
+  group_by(industry)|>
+  nest()|>
+  rename(driver_data=data)|>
+  fuzzy_right_join(
+    employment,
+    by_x = "industry",
+    by_y = "industry",
+    max_dist = 2
+  )
+
+#names (if any) should refer to the same industry
+driver_data_wrong_names[driver_data_wrong_names$x.industry!=driver_data_wrong_names$y.industry,]
+
+notes_wrong_names <- read_view("notes.xlsx")|>
+  select(industry=contains("name"), starts_with("2"))|>
+  fuzzy_right_join(
+    employment,
+    by_x = "industry",
+    by_y = "industry",
+    max_dist = 2
+  )
+
+#names (if any) should refer to the same industry
+notes_wrong_names[notes_wrong_names$x.industry!=notes_wrong_names$y.industry,]
+
+
+budget_constraint <- read_view("constraint.xlsx") #no industry names
+
+rich_fcast <- read_view("richs_forecast.xlsx")|>
+  group_by(lmo_ind_code, lmo_detailed_industry, year)|>
+  summarize(value=sum(employment))|>
+  unite(industry, lmo_ind_code, lmo_detailed_industry, sep=": ")
+
+census <- read_view("census_industry.xlsx")|>
   unite(industry, lmo_ind_code, lmo_detailed_industry, sep=": ")|>
-  select(-value)|>
+  rename(value=employment)
+
+census$industry <- clean(census$industry)
+
+# clean up after fuzzyjoins
+
+old_forecast <- old_forecast_wrong_names|>
+  ungroup()|>
+  select(x.old_forecast, y.code, y.industry)|>
+  unnest(x.old_forecast)|>
+  unite(industry, y.code, y.industry, sep=": ")|>
+  mutate(year=as.numeric(year))
+
+driver_data <- driver_data_wrong_names|>
+  ungroup()|>
+  select(x.driver_data, y.code, y.industry)|>
+  unnest(x.driver_data)|>
+  unite(industry, y.code, y.industry, sep=": ")|>
   mutate(year=as.numeric(year),
-         thing="historic")|>
-  tsibble(key=industry, index = year)
+         mean_value=mean(value, na.rm = TRUE),
+         value=if_else(mean_value<1000, value*1000, value) #if mean value < 1000 must be in 1000's
+  )|>
+  select(-mean_value)
 
-# fit some models on the proportion data
-fit <- tbbl |>
-  model(linear=TSLM(prop ~ trend()), #linear trend model
-        ets=ETS(prop) #exponential smoothing
-        )
+notes <- notes_wrong_names|>
+  select(contains("20"), y.code, y.industry)|>
+  unite(industry, c(y.code, y.industry), sep=": ")|>
+  pivot_longer(cols=contains("20"))|>
+  mutate(value=str_replace_all(value, "-"," "),
+         name = stringr::str_extract(name, "\\b\\d{4} Edition\\b"))
 
-# forecast the models
-forecast <- forecast(fit, h = 11)
 
-#take the mean of the normalized proportion forecasts-------------------------
+#write to rds files----------------
 
-mean_prop <- forecast|>
-  as_tibble()|>
-  group_by(industry, year)|>
-  summarize(mean_fcast=mean(.mean))|>  #the mean raw proportion forecasts
-  group_by(year)|>
-  mutate(mean_fcast=mean_fcast/sum(mean_fcast)) #normalize the proportions to sum to 1.
-
-write_csv(mean_prop, here("data","current","prop_fcast.csv"))
-
-#take a look--------------------------------
-
-mean_prop|>
-  rename(prop=mean_fcast)|>
-  mutate(thing="forecast")|>
-  bind_rows(tbbl)|>
-  mutate(industry=str_trunc(industry, 20))|>
-  ggplot(aes(year, prop, colour = thing))+
-  geom_line()+
-  facet_wrap(~industry, scales="free")
-
+write_rds(census, here("app_data", "census.rds"))
+write_rds(budget_constraint, here("app_data", "budget_constraint.rds"))
+write_rds(driver_data, here("app_data", "driver_data.rds"))
+write_rds(employment, here("app_data", "employment.rds"))
+write_rds(notes, here("app_data", "notes.rds"))
+write_rds(old_forecast, here("app_data", "old_forecast.rds"))
+write_rds(rich_fcast, here("app_data", "rich_fcast.rds"))
 
 
